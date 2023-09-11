3405c4d26ebfea1d0ba0c296cb9017fdef1528a6
Vanya Maheshwari <vanimaheshwari19@gmail.com>
2023-09-11 17:36:45 +0530

    moment added

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get('/', (req, res) =>{
     // res.json(memebers);
 // })
 
+// To get the URL after sending the get request in Postman, you'll get the output as the URL link of the website and the time format from moment()
+
+// const logger = (req, res, next) =>{
+//     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`)
+//     next();
+// }
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, ()=> console.log(`Server started on PORT ${PORT}`)); 
